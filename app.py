ebf2d6caf4300e27cd1165e8164f5892b2d66ac6
Aryane <aryanebueno70@gmail.com>
2025-07-24 17:19:31 -0300

    Commit Pietro

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -6,10 +6,37 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.get("/formulario")
-def form():
+@app.get("/formulargit
     return render_template("formulario.html")
 
+@app.route("/cardapio", methods=["Post"]) 
+ def cardapio() 
+  restricoes= request.form.getlist
+  ("restricoes")
+
+  pratos = [
+     {
+      "nome": "feijoada" 
+      "imagem" : "feijoada.jpg"
+      "restricoes" : []
+      },
+      {
+         "nome" : "salada Vegetariana"
+         "imagem" : "salada.jpg"
+         "restricoes" : ["Vegano", "Vegetariano", "Leite", "Gluten"]
+      },
+      {
+         "nome" : "lasanha"
+         "imagem" : "lasanha.jpg"
+         "restricoes": ["Vegetariano", "Leite", "Gluten"]
+      },
+      {
+         "nome" : "Hamburguer Vegano"
+         "imagem" : "Hamburguer.jpg"
+         "restricoes" : ["Gluten", "Vegano", "Vegetariano", "Leite"]
+      }
+       
+        ]
 
 if __name__ == '__main__':
     app.run(debug=True)
